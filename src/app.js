ec85099a5c2e21dd4a03c9fea06c904fade1f63e
Qaid Jacobs <projects.qaid@gmail.com>
2016-02-04 15:40:17 +0100

    Add markup to static teams page

--- src/app.js
+++ src/app.js
@@ -2,7 +2,20 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Hackathon Planner';
     config.map([
-      { route: [''],   name: 'ideas',          moduleId: 'ideas',      nav: false, title: 'Hackathon Ideas' }
+      {
+        route: [''],
+        name: 'ideas',
+        moduleId: 'ideas',
+        nav: false,
+        title: 'Hackathon Ideas'
+      },
+      {
+        route: ['teams'],
+        name: 'teams',
+        moduleId: 'teams',
+        nav: false,
+        title: 'Hackathon Teams'
+      }
     ]);
 
     this.router = router;
