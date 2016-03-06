2f01c51dc827e871b5a859aa0fa372225de73580
hakant <hakantuncer@gmail.com>
2016-03-06 15:47:32 +0100

    Remove like and join handlers from overview and jspm update all packages

--- src/idea-card.js
+++ src/idea-card.js
@@ -2,15 +2,46 @@ import {inject} from "aurelia-framework";
 import {bindable} from "aurelia-framework";
 import {Router} from 'aurelia-router';
 
-@inject (Router)
-export class IdeaCard{
-  @bindable project = null;
+@inject(Router)
+export class IdeaCard {
+    @bindable project = null;
 
-  constructor(router){
-    this.router = router;
-  }
+    constructor(router) {
+        this.router = router;
+    }
 
-  goToCardDetails(event){
-    this.router.navigateToRoute('ideaDetail', { id: this.project._id });
-  }
+    goToCardDetails(event) {
+        this.router.navigateToRoute('ideaDetail', { id: this.project._id });
+    }
+
+    like(event) {
+        let projectId = this.project._id;
+
+        for (let item of window._projects) {
+            if (item._id == projectId) {
+                item.liked = !item.liked;
+            }
+        }
+    }
+
+    join(event) {
+        let projectId = this.project._id;
+
+        for (let item of window._projects) {
+            if (item.joined) {
+                item.joined = false;
+
+                if (item._id == projectId)
+                    return;
+            }
+
+            if (item._id == projectId) {
+                if (item.joined) {
+                    item.joined = false;
+                } else {
+                    item.joined = true;
+                }
+            }
+        }
+    }
 }
--- src/overview.js
+++ src/overview.js
@@ -17,35 +17,4 @@ export class Overview {
         this.http = http;
         this._projects = window._projects;
     }
-
-    like(event) {
-        let projectId = event.srcElement.dataset.projectid;
-
-        for (let item of this._projects) {
-            if (item._id == projectId) {
-                item.liked = !item.liked;
-            }
-        }
-    }
-
-    join(event) {
-        let projectId = event.srcElement.dataset.projectid;
-
-        for (let item of this._projects) {
-            if (item.joined) {
-                item.joined = false;
-
-                if (item._id == projectId)
-                    return;
-            }
-
-            if (item._id == projectId) {
-                if (item.joined) {
-                    item.joined = false;
-                } else {
-                    item.joined = true;
-                }
-            }
-        }
-    }
 }
