4cd657a3b88495ff8bec3c09bd2f5b45b0b50568
hakant <hakantuncer@gmail.com>
2016-04-19 22:33:15 +0200

    update tooltip content

--- src/data/tooltips.js
+++ src/data/tooltips.js
@@ -5,7 +5,7 @@ export class Tooltips {
         tips: [{
             select: ".navbar",
             title: "Welcome to Hackathon Planner!",
-            content: "These tips are designed to help you get up and running in seconds and then they'll get out of the way. Click on the tooltips to move forward.",
+            content: "These tips are designed to help you get up and running in seconds and then they'll get out of the way. Click or tap on a tooltip to move forward.",
             placement: "bottom"
         }, {
                 select: "button",
@@ -26,7 +26,7 @@ export class Tooltips {
                 select: "div.card:first i:last",
                 title: "Like an Idea",
                 content: "Hackathon Projects need likes. Only if they get enough likes, the project owner will be able to pitch the idea on the Hackathon day. So if you like a project idea, do not hesitate to like it!",
-                placement: "bottom"
+                placement: "left"
             }]
     },
         {
@@ -40,7 +40,8 @@ export class Tooltips {
                     select: "#new-card textarea.card-overview",
                     title: "Project Overview",
                     content: "Overview is displayed directly on the hackathon idea cards and they can't be longer than 300 chars. You'll also get a chance to write a longer description for your project so make sure the overview doesn't go into too many details.",
-                    placement: "bottom"
+                    placement: "bottom",
+                    offset: "50% 0"
                 }]
         },
         {
@@ -54,7 +55,7 @@ export class Tooltips {
                 {
                     select: "div.card-text",
                     title: "Project Description in MarkDown",
-                    content: "Project description is where the Hackathon Projects are discussed in depth. Make sure you use the full power of mark down.",
+                    content: "Project descriptions are where Hackathon Projects are discussed in depth. Make sure you use the full power of mark down.",
                     placement: "top"
                 }]
         }];
