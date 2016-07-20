4b4d5c411722d59c5ed9cc27790447859c2e1c21
Rutger de Jong <r.de.jong@niposoftware.com>
2016-07-21 00:23:51 +0200

    Add jpg images to bundle task

--- build/paths.js
+++ build/paths.js
@@ -8,6 +8,7 @@ module.exports = {
   html: appRoot + '**/*.html',
   json: appRoot + '**/*.json',
   css: appRoot + '**/*.css',
+  image: appRoot + '**/*.jpg',
   style: 'styles/**/*.css',
   output: outputRoot,
   exportSrv: exportSrvRoot,
